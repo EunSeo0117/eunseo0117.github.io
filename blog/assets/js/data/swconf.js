6693e9cc7262c1c9dba7e9e4b8bd7cb22bb102b5
EunSeo0117 <EunSeo0117@users.noreply.github.com>
2025-09-30 14:15:45 +0000

    deploy: EunSeo0117/blog@1b31c6394f7ab870096df94739b9b27755b6a30f

--- blog/assets/js/data/swconf.js
+++ blog/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1759144787',resources: [
+    cacheName: 'chirpy-1759241741',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
